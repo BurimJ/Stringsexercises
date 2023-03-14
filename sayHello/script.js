08088e0c597f6a24929bc7e852d68806daf90b4a
BurimJ <ipyroburnz@gmail.com>
2023-03-14 13:50:33 +0100

    update

---
+++ sayHello/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const firstName = "Burim";
+const lastName = "Jusufi";
+
+function sayHello(firstName, lastName) {
+  console.log(`Hello ${lastName} ${firstName}`);
+}
+
+sayHello(firstName, lastName);
+
+// sayHello("Pter");
+// sayHello("Burim J");
+// sayHello("Noget ");
